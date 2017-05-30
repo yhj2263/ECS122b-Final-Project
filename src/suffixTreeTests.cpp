c0c2ed129a21052c283d128f8602875ddefa9028
YHJ <yhj2263>
2017-05-30 00:30:38 -0700

    add .gitignore and CMakeLists.txt, create a empty test

---
+++ src/suffixTreeTests.cpp
@@ -0,0 +1,13 @@
+#include "suffixTree.h"
+#include "gtest/gtest.h"
+
+using testing::Test;
+
+TEST(EmptyTest, NoMatchTest) {
+  // TODO: Implement this
+}
+
+int main(int argc, char** argv) {
+   testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
+}
